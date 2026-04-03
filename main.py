123cfbc7068b6a87668de5eeb759e279471656db
PaperBag <92173476+Paper-Bag-dev@users.noreply.github.com>
2026-04-03 06:31:10 +0530

    Rename FastAPI title from 'ETL Backend Assignment' to 'ETL Ingestion Service'

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ async def lifespan(app: FastAPI):
         print("Scheduler stopped")
 
 
-app = FastAPI(title="ETL Backend Assignment", lifespan=lifespan)
+app = FastAPI(title="ETL Ingenstion Service", lifespan=lifespan)
 
 app.middleware("http")(request_context_middleware)
 app.include_router(health_router)
